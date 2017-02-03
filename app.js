9d6fbf21f4603d83fe66fd31cd79abf320a91158
Matt Dean <matt.dean.001@gmail.com>
2017-02-03 13:05:59 +0000

    return next

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(function (req, res, next) {
   if (!req.session) {
     sessionMiddleware(req, res, next);
   }
-  next(); // otherwise continue
+  return next(); // otherwise continue
 });
 
 
