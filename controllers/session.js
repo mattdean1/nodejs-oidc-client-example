94403b256975189805e4d96172efedac82f0f54d
Matt Dean <matt.dean.001@gmail.com>
2017-02-06 09:55:36 +0000

    re-add mongo connection check on session initialisation

--- controllers/session.js
+++ controllers/session.js
@@ -1,6 +1,20 @@
 var session = require('express-session');
 var MongoDBStore = require('connect-mongodb-session')(session);
+var MongoClient = require('mongodb').MongoClient;
+
 var sessionStore;
+
+//setup session options
+var sessionOptions = {
+  saveUninitialized: true, // saved new sessions
+  resave: false, // do not automatically write to the session store
+  store: sessionStore,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    maxAge: 1800000,
+  },
+};
 
 //setup mongo connection values
 var user = process.env.MONGODB_USER;
@@ -13,29 +27,23 @@ var database = process.env.MONGODB_DATABASE;
 if(user && password && host && port && database){
   var connectionstring = user + ":" + password + "@" + host + ":" + port + "/" + database;
 
-    sessionStore = new MongoDBStore(
-    {
-        uri: 'mongodb://' + connectionstring,
-        collection: 'sessions'
-    }
-  );
-  // Catch errors
-  sessionStore.on('error', function(error) {
-    console.log('Mongo session error: ' + error);
+  MongoClient.connect("mongodb://"+connectionstring, function(err, db) {
+      if(err){
+        console.log("Can't connect to mongoDB using the provided host information and credentials, falling back to use MemoryStore for session storage");
+      }
+      else {
+        sessionStore = new MongoDBStore(
+          {
+              uri: 'mongodb://' + connectionstring,
+              collection: 'sessions'
+          },
+          function(e){
+            //catch error
+          }
+        );
+      }
   });
 }
 
-
-
-var sessionOptions = {
-  saveUninitialized: false, // saved new sessions
-  resave: false, // do not automatically write to the session store
-  store: sessionStore,
-  secret: process.env.COOKIE_SECRET,
-  cookie: {
-    httpOnly: true,
-    maxAge: 1800000,
-  },
-};
 
 module.exports = session(sessionOptions);
